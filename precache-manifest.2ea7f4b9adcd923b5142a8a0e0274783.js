c32b236452d1e7908de1c62db3fbb6a0a4a3324b
Tom Corke <tom@tomcorke.com>
2019-06-13 17:07:52 +0100

    Updates

---
+++ precache-manifest.2ea7f4b9adcd923b5142a8a0e0274783.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6d49dd7c789a41d4f256de47cec8bf0",
+    "url": "/destiny-power-bars/index.html"
+  },
+  {
+    "revision": "670afb52191369a4b101",
+    "url": "/destiny-power-bars/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "0029fa558b615757b8fc",
+    "url": "/destiny-power-bars/static/js/2.ca79993d.chunk.js"
+  },
+  {
+    "revision": "670afb52191369a4b101",
+    "url": "/destiny-power-bars/static/js/main.6e6c2df5.chunk.js"
+  },
+  {
+    "revision": "43444d8756ced8d33826",
+    "url": "/destiny-power-bars/static/js/runtime~main.5c018d2b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.24d8d004cde8811a1ca12efdb58d5f8b.js"
+  "/destiny-power-bars/precache-manifest.2ea7f4b9adcd923b5142a8a0e0274783.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/destiny-power-bars/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
