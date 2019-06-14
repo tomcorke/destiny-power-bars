1be4effc6b46645bde90ad7efae11ef1a2afdbdb
Tom Corke <tom@tomcorke.com>
2019-06-14 01:16:11 +0100

    Updates

--- precache-manifest.ff61ef4a673cf19c2d12d2bd48368ec5.js
+++ precache-manifest.ff61ef4a673cf19c2d12d2bd48368ec5.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "565bc20b5112c752de871873fe74452e",
+    "revision": "b6f51b0fa1519f19c5f43588682cf770",
     "url": "/destiny-power-bars/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/destiny-power-bars/static/css/2.764ccc25.chunk.css"
   },
   {
-    "revision": "b113af89486589f3b50f",
+    "revision": "fa85ed34bf33d0325bc7",
     "url": "/destiny-power-bars/static/css/main.36f8d3d3.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/destiny-power-bars/static/js/2.ec65f63f.chunk.js"
   },
   {
-    "revision": "b113af89486589f3b50f",
-    "url": "/destiny-power-bars/static/js/main.3557ec17.chunk.js"
+    "revision": "fa85ed34bf33d0325bc7",
+    "url": "/destiny-power-bars/static/js/main.ae779f95.chunk.js"
   },
   {
     "revision": "43444d8756ced8d33826",
