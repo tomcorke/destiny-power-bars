d2cfbc3ca49127088fcb0c296d50bcef2c2b38f9
Tom Corke <tom@tomcorke.com>
2019-11-10 17:44:27 +0000

    Make inter-power numbers semi-transparent

--- src/components/characterDisplay/InterPowerBar.stories.tsx
+++ src/components/characterDisplay/InterPowerBar.stories.tsx
@@ -1,26 +1,36 @@
 import React from "react";
 
+import { CharacterDisplayBodyWrapper } from "../CharacterDisplay";
 import { InterPowerBar } from "./InterPowerBar";
 
 export default {
-  title: "Inter-Power Bar Display"
+  title: "Inter-Power Bar Display",
+  decorators: [
+    (storyFn: () => JSX.Element) =>
+      CharacterDisplayBodyWrapper("rgb(7, 5, 43)", storyFn())
+  ]
 };
 
 const withMaxSize = (size: number, element: JSX.Element) => (
-  <div style={{ width: "100%", maxWidth: `${size}px` }}>{element}</div>
+  <div
+    style={{
+      width: "100%",
+      maxWidth: `${size}px`,
+      padding: "2px 5px",
+      boxSizing: "border-box"
+    }}
+  >
+    {element}
+  </div>
 );
 
-export const display = () => <InterPowerBar value={950.8} />;
+export const display = () => withMaxSize(800, <InterPowerBar value={950.8} />);
 
 export const atDifferentSizes = () => (
   <>
     {withMaxSize(200, <InterPowerBar value={950 + 7 / 8} />)}
     {withMaxSize(300, <InterPowerBar value={950 + 7 / 8} />)}
     {withMaxSize(400, <InterPowerBar value={950 + 7 / 8} />)}
-    {withMaxSize(500, <InterPowerBar value={950 + 7 / 8} />)}
-    {withMaxSize(600, <InterPowerBar value={950 + 7 / 8} />)}
-    {withMaxSize(700, <InterPowerBar value={950 + 7 / 8} />)}
-    {withMaxSize(800, <InterPowerBar value={950 + 7 / 8} />)}
   </>
 );
 export const atDifferentValues = () => (
