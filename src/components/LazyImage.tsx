d2260cbb5cabb45064def60c3f807678dbf0dd76
Tom Corke <tom@tomcorke.com>
2019-11-25 22:03:39 +0000

    Add LazyImage component

---
+++ src/components/LazyImage.tsx
@@ -0,0 +1,48 @@
+import classnames from "classnames";
+import React, { useCallback, useState } from "react";
+
+import STYLES from "./LazyImage.module.scss";
+
+interface LazyImageProps {
+  lowResImage: string;
+  highResImage: string;
+  delay?: number;
+  alt?: string;
+}
+
+export const LazyImage = ({
+  lowResImage,
+  highResImage,
+  alt = ""
+}: LazyImageProps) => {
+  const [highResImageLoaded, setHighResImageLoaded] = useState(false);
+  const [lowResImageAsOverlay, setLowResImageAsOverlay] = useState(false);
+
+  const onHighResImageLoad = useCallback(() => {
+    setHighResImageLoaded(true);
+  }, [setHighResImageLoaded]);
+
+  const result = (
+    <div className={STYLES.lazyImage}>
+      <img
+        src={highResImage}
+        alt={alt}
+        className={classnames({
+          [STYLES.hidden]: !highResImageLoaded,
+          [STYLES.overlay]: !highResImageLoaded
+        })}
+        onLoad={onHighResImageLoad}
+      />
+      <img
+        src={lowResImage}
+        alt={alt}
+        className={classnames({
+          [STYLES.hidden]: highResImageLoaded,
+          [STYLES.overlay]: highResImageLoaded
+        })}
+      />
+    </div>
+  );
+
+  return result;
+};
