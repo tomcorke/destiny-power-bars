0b55b737071c6a462152592a4758e67dcdde84b0
Tom Corke <tom.corke@skyscanner.net>
2019-12-03 15:04:13 +0000

    Comments should not be plain text in output

--- src/components/characterDisplay/CharacterLinks.tsx
+++ src/components/characterDisplay/CharacterLinks.tsx
@@ -17,7 +17,7 @@ const link = (url: string, { text, tags = [] }: LinkOptions = {}) => {
   const linkText = text || new URL(url).hostname;
   return (
     <li>
-      /* eslint-disable-next-line react/jsx-no-target-blank */
+      {/* eslint-disable-next-line react/jsx-no-target-blank */}
       <a
         href={url}
         target="_blank"
