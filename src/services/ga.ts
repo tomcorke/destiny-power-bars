fcc9edf0952d488e975fefe52d4e0bd90fc26d3e
Tom Corke <tom@tomcorke.com>
2019-12-13 11:08:16 +0000

    Allow disabling (and disable) GA tracking

--- src/services/ga.ts
+++ src/services/ga.ts
@@ -1,11 +1,37 @@
 import ReactGA from "react-ga";
 
-ReactGA.initialize("UA-149614480-1", {
-  // debug: process.env.NODE_ENV === "development"
-});
+const ENABLED = false;
 
-// Anonymize IP addresses, yay GDPR
-ReactGA.set({ anonymizeIp: true });
-ReactGA.pageview(window.location.pathname + window.location.search);
+interface AnalyticsInterface {
+  set: (props: { [key: string]: string | boolean }) => void;
+  initialize: (clientId: string, options: { debug?: boolean }) => void;
+  pageview: (url: string) => void;
+  event: (eventProps: {
+    category: string;
+    action: string;
+    label?: string;
+    nonInteraction?: boolean;
+  }) => void;
+}
 
-export default ReactGA;
+let ga: AnalyticsInterface = ReactGA;
+
+if (ENABLED) {
+  ga.initialize("UA-149614480-1", {
+    // debug: process.env.NODE_ENV === "development"
+  });
+
+  // Anonymize IP addresses, yay GDPR
+  ga.set({ anonymizeIp: true });
+  ga.pageview(window.location.pathname + window.location.search);
+} else {
+  // Set mock GA object to disable without removing other code
+  ga = {
+    initialize: () => undefined,
+    pageview: () => undefined,
+    set: (...args: any[]) => undefined,
+    event: (...args: any[]) => undefined
+  };
+}
+
+export default ga;
