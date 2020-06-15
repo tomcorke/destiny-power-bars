0b41069ea4750b48b70caca95a40a8049cda0071
joshhunt <joshhunt@users.noreply.github.com>
2020-06-15 16:13:06 +0100

    Add manual refresh button to do a cheeky profile cache bust

--- src/components/CharacterDisplay.stories.tsx
+++ src/components/CharacterDisplay.stories.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { PowerBarsCharacterData } from "../types";
 import CharacterDisplay from "./CharacterDisplay";
 import { characterDataSnapshot } from "./CharacterDisplayMockData";
+import { DestinyItemComponent } from "bungie-api-ts/destiny2";
 
 export default { title: "Character Display" };
 
@@ -19,10 +20,10 @@ const mockData: PowerBarsCharacterData = {
       red: 7,
       green: 5,
       blue: 43,
-      alpha: 255
+      alpha: 255,
     },
     emblemBackgroundPath:
-      "/common/destiny2_content/icons/3d0142ffcf985e9fab3d3e7d89ec192c.jpg"
+      "/common/destiny2_content/icons/3d0142ffcf985e9fab3d3e7d89ec192c.jpg",
   },
 
   className: "warlock",
@@ -36,114 +37,114 @@ const mockData: PowerBarsCharacterData = {
       itemHash: 1,
       instanceData: {
         primaryStat: {
-          value: 942
-        }
+          value: 942,
+        },
       },
       itemDefinition: {
         displayProperties: {
           icon:
-            "/common/destiny2_content/icons/a622e55eb4a599a6b16d9b8b2f74ca73.jpg"
-        }
-      }
+            "/common/destiny2_content/icons/a622e55eb4a599a6b16d9b8b2f74ca73.jpg",
+        },
+      },
     },
     energy: {
       itemHash: 1,
       instanceData: {
         primaryStat: {
-          value: 940
-        }
+          value: 940,
+        },
       },
       itemDefinition: {
         displayProperties: {
           icon:
-            "/common/destiny2_content/icons/a622e55eb4a599a6b16d9b8b2f74ca73.jpg"
-        }
-      }
+            "/common/destiny2_content/icons/a622e55eb4a599a6b16d9b8b2f74ca73.jpg",
+        },
+      },
     },
     power: {
       itemHash: 1,
       instanceData: {
         primaryStat: {
-          value: 938
-        }
+          value: 938,
+        },
       },
       itemDefinition: {
         displayProperties: {
           icon:
-            "/common/destiny2_content/icons/a622e55eb4a599a6b16d9b8b2f74ca73.jpg"
-        }
-      }
+            "/common/destiny2_content/icons/a622e55eb4a599a6b16d9b8b2f74ca73.jpg",
+        },
+      },
     },
     head: {
       itemHash: 1,
       instanceData: {
         primaryStat: {
-          value: 936
-        }
+          value: 936,
+        },
       },
       itemDefinition: {
         displayProperties: {
           icon:
-            "/common/destiny2_content/icons/a622e55eb4a599a6b16d9b8b2f74ca73.jpg"
-        }
-      }
+            "/common/destiny2_content/icons/a622e55eb4a599a6b16d9b8b2f74ca73.jpg",
+        },
+      },
     },
     gloves: {
       itemHash: 1,
       instanceData: {
         primaryStat: {
-          value: 934
-        }
+          value: 934,
+        },
       },
       itemDefinition: {
         displayProperties: {
           icon:
-            "/common/destiny2_content/icons/a622e55eb4a599a6b16d9b8b2f74ca73.jpg"
-        }
-      }
+            "/common/destiny2_content/icons/a622e55eb4a599a6b16d9b8b2f74ca73.jpg",
+        },
+      },
     },
     chest: {
       itemHash: 1,
       instanceData: {
         primaryStat: {
-          value: 924
-        }
+          value: 924,
+        },
       },
       itemDefinition: {
         displayProperties: {
           icon:
-            "/common/destiny2_content/icons/a622e55eb4a599a6b16d9b8b2f74ca73.jpg"
-        }
-      }
+            "/common/destiny2_content/icons/a622e55eb4a599a6b16d9b8b2f74ca73.jpg",
+        },
+      },
     },
     legs: {
       itemHash: 1,
       instanceData: {
         primaryStat: {
-          value: 922
-        }
+          value: 922,
+        },
       },
       itemDefinition: {
         displayProperties: {
           icon:
-            "/common/destiny2_content/icons/a622e55eb4a599a6b16d9b8b2f74ca73.jpg"
-        }
-      }
+            "/common/destiny2_content/icons/a622e55eb4a599a6b16d9b8b2f74ca73.jpg",
+        },
+      },
     },
     classItem: {
       itemHash: 1,
       instanceData: {
         primaryStat: {
-          value: 920
-        }
+          value: 920,
+        },
       },
       itemDefinition: {
         displayProperties: {
           icon:
-            "/common/destiny2_content/icons/a622e55eb4a599a6b16d9b8b2f74ca73.jpg"
-        }
-      }
-    }
+            "/common/destiny2_content/icons/a622e55eb4a599a6b16d9b8b2f74ca73.jpg",
+        },
+      },
+    },
   },
   artifactData: {
     bonusPower: 12,
@@ -151,8 +152,8 @@ const mockData: PowerBarsCharacterData = {
       "/common/destiny2_content/icons/a622e55eb4a599a6b16d9b8b2f74ca73.jpg",
     name: "Test Artifact",
     nextLevelAt: 100,
-    progressToNextLevel: 97
-  }
+    progressToNextLevel: 97,
+  },
 };
 
 export const mockWarlock = () => <CharacterDisplay data={mockData} />;
