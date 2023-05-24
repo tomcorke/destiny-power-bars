b1083ce852132f0dbe26119d4417bc207856e108
tomcorke <tom@tomcorke.com>
2023-05-24 09:02:51 +0100

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,16 @@ import { defineConfig } from "vite";
 import svgrPlugin from "vite-plugin-svgr";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import BuildInfo from "vite-plugin-info";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin(), BuildInfo()],
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    svgrPlugin(),
+    BuildInfo(),
+    basicSsl(),
+  ],
   envDir: "./",
 });
