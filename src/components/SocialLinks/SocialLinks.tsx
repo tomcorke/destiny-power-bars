74d96c107b53498cf2718024ad35f2178a49059a
Tom Corke <tom@tomcorke.com>
2025-02-18 09:33:45 +0000

    Remove twitter link

--- src/components/SocialLinks/SocialLinks.tsx
+++ src/components/SocialLinks/SocialLinks.tsx
@@ -13,18 +13,6 @@ const SocialLinks = () => (
     </div>
     <a
       className={STYLES.link}
-      href="https://www.twitter.com/corke_tom"
-      target="_blank"
-      rel="noreferrer noopener"
-    >
-      <TwitterLogo
-        width={20}
-        height={20}
-        style={{ fill: "rgba(255,255,255,0.8)" }}
-      />
-    </a>
-    <a
-      className={STYLES.link}
       href="https://github.com/tomcorke/destiny-power-bars"
       target="_blank"
       rel="noreferrer noopener"
