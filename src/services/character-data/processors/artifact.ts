b014c8917213e6479ce7b4b136e8d90b0867e75e
Tom Corke <tom@tomcorke.com>
2025-08-04 17:45:47 +0100

    remove artifact power

--- src/services/character-data/processors/artifact.ts
+++ src/services/character-data/processors/artifact.ts
@@ -18,32 +18,32 @@ export default createGlobalDataProcessor(
       artifactItemComponent?.itemInstanceId &&
       itemInstances[artifactItemComponent.itemInstanceId];
 
-    if (
-      artifactItemComponent &&
-      profileProgression.seasonalArtifact &&
-      artifactInstance
-    ) {
-      const artifactDefinition =
-        manifest.DestinyInventoryItemDefinition[
-          artifactItemComponent.itemHash
-        ]!;
+    // if (
+    //   artifactItemComponent &&
+    //   profileProgression.seasonalArtifact &&
+    //   artifactInstance
+    // ) {
+    //   const artifactDefinition =
+    //     manifest.DestinyInventoryItemDefinition[
+    //       artifactItemComponent.itemHash
+    //     ]!;
 
-      const artifactData = {
-        bonusPower:
-          profileProgression.seasonalArtifact.powerBonus ||
-          artifactInstance.primaryStat.value,
-        iconPath: artifactDefinition.displayProperties?.icon,
-        name: artifactDefinition.displayProperties?.name || "Artifact",
-        progressToNextLevel:
-          profileProgression.seasonalArtifact?.powerBonusProgression
-            ?.progressToNextLevel,
-        nextLevelAt:
-          profileProgression.seasonalArtifact?.powerBonusProgression
-            ?.nextLevelAt,
-      };
+    //   const artifactData = {
+    //     bonusPower:
+    //       profileProgression.seasonalArtifact.powerBonus ||
+    //       artifactInstance.primaryStat.value,
+    //     iconPath: artifactDefinition.displayProperties?.icon,
+    //     name: artifactDefinition.displayProperties?.name || "Artifact",
+    //     progressToNextLevel:
+    //       profileProgression.seasonalArtifact?.powerBonusProgression
+    //         ?.progressToNextLevel,
+    //     nextLevelAt:
+    //       profileProgression.seasonalArtifact?.powerBonusProgression
+    //         ?.nextLevelAt,
+    //   };
 
-      return { artifact: artifactData };
-    }
+    //   return { artifact: artifactData };
+    // }
 
     return { artifact: undefined };
   }
