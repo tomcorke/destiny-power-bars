0841db69252bd26437905e67ad7e0c18fad98ced
Tom Corke <tom@tomcorke.com>
2025-08-04 17:05:33 +0100

    Update d2ai-module and re-enable power hints

--- src/constants.ts
+++ src/constants.ts
@@ -77,10 +77,10 @@ export const ITEM_POWER_SOFT_CAP = season.softCap;
 export const ITEM_POWER_POWERFUL_CAP = season.powerfulCap;
 export const ITEM_POWER_PINNACLE_CAP = season.pinnacleCap;
 
-export const LEGENDARY_STORY_ITEM_POWER = 1960;
+export const LEGENDARY_STORY_ITEM_POWER = 100;
 export const LEGENDARY_ACTIVITY_MODIFIER_HASH = 36103086;
 export const LEGENDARY_ACTIVITY_MODIFIER_ICON =
-  "https://www.bungie.net/common/destiny2_content/icons/ce7c212b48151d8075cfba5ecc3934c8.png";
+  "https://www.bungie.net/common/destiny2_content/icons/07c0a5825aedb265bc69248604fb07cb.png";
 
 // How much above your current power do pinnacles drop when above the powerful cap
 export const ITEM_POWER_PINNACLE_DROP_OFFSET_WEAK = 1;
